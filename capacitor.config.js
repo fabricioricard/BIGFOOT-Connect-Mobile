472d11385c2c1c0da1dc45c5318905320be18194
Fabricio Ricard <fabricioricard23@gmail.com>
2025-09-13 18:20:39 -0300

    Fix package.json and capacitor config for AppFlow

---
+++ capacitor.config.js
@@ -0,0 +1,13 @@
+const config = {
+  appId: 'com.bigfoot.network',
+  appName: 'BIGFOOT Network',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  android: {
+    allowMixedContent: true
+  }
+};
+
+module.exports = config;
